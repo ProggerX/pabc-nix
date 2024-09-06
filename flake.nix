4cc07c5b839b01c380f3a4ef47777bc5b5b84f90
ProggerX <x@proggers.ru>
2024-09-06 13:10:51 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs";
+        flake-parts.url = "github:hercules-ci/flake-parts";
+    };
+    outputs = { nixpkgs, flake-parts, ... }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
+        systems = nixpkgs.lib.platforms.unix;
+        perSystem = { pkgs, ... }: {
+            packages.default =
+            let pabcnetc-zip = pkgs.fetchurl {
+                url = "https://pascalabc.net/downloads/PABCNETC.zip";
+                hash = "sha256-i4pH+VSyeP4Fbt+yPYb6oGw4CE7/8N1p0osx4jwsKAk=";
+            };
+            pabcnetc = pkgs.stdenv.mkDerivation {
+                name = "pabcnetc";
+                src = pabcnetc-zip;
+                phases = [ "installPhase" ];
+                installPhase = ''
+                    mkdir $out
+                    cd $out
+                    ${pkgs.p7zip}/bin/7z x -y $src
+                '';
+            };
+            in pkgs.writeShellScriptBin "pabc-run" ''
+                ${pkgs.mono}/bin/mono ${pabcnetc}/pabcnetc.exe $1 && ${pkgs.mono}/bin/mono ''${1%.*}.exe && rm ''${1%.*}.exe ''${1%.*}.exe.mdb
+            '';
+        };
+    };
+}
